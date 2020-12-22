54ee78bc58849d6faa813341d91552c3305900f7
Alie Stone <helloaliestone@gmail.com>
2020-12-22 00:40:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post 'piglatinize' do
+    erb :user_input
+  end
+
+
+  post '/' do
+    erb :piglatinizer
+  end
+
+
+
 end
